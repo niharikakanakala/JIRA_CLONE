9518e5b7aad1e758861fc44c72ed29c1b639e1ec
Niharika Kanakala <niharikakanakala@gmail.com>
2025-08-05 02:48:50 +0530

    setting up tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,16 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'mobile': {'max': '499px'},
+        'tablet': {'min': '500px', 'max': '768px'},
+        'desktop': {'min': '769px'}
+      }
+    },
   },
   plugins: [],
 }
